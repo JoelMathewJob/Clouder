63f6a29738bf882b2c416e2dd428238d11ce956e
Joel Mathew Job <joelmathewjob36@gmail.com>
2024-07-04 02:45:51 +0530

    updated vite.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 
 export default defineConfig({
   plugins: [svelte()],
+  base: '/Clouder/', // Set this to the name of your GitHub repository
   build: {
     outDir: 'build'
   }
